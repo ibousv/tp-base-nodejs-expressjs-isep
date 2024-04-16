ac2c0a4728e4c3e25943568bc3a2eb8d69c01288
Amadou Lamine NDIAYE <lzeu95@hotmail.com>
2024-04-16 09:01:04 +0000

    params

--- 05-params-query.js
+++ 05-params-query.js
@@ -5,7 +5,26 @@ const  PORT = 5000;
 const app = express();
 
 app.get('/api/v1/produits', (req, res) => {
-	res.json(produits);
+	const newProducts = produits.map((product) => {
+		const {id, name, price} = product;
+
+		return {id, name, price};
+	})
+	
+	res.json(newProducts);
+});
+
+app.get('/api/v1/produits/:id', (req, res) => {
+	const id = req.params.id
+	console.log(id);
+	
+	const singleProduct = produits.find((product)=> product.id === Number(id));
+	
+	if (!singleProduct) {
+		res.status(404).send("aucun produit avec id " + id)
+	}
+	
+	res.json(singleProduct);
 });
 
 app.listen(PORT, ()=> {
